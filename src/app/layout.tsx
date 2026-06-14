a7e95e449cefc57a247be71329bcf5b73b9a22d3
akshaygupta24042007-spec <akshaygupta24042007@gmail.com>
2026-06-15 00:14:47 +0530

    chore: add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -199,6 +199,20 @@ export default function RootLayout({
         />
         {/* End Google tag */}
 
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "x71ef22e2e");
+            `
+          }}
+        />
+        {/* End Microsoft Clarity */}
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
