1ad1330563825ad54e5d36b7ba0813a4a2a4e1b8
akshaygupta24042007-spec <akshaygupta24042007@gmail.com>
2026-08-08 16:11:36 +0530

    Fix JSX syntax error caused by missing fragment tags

--- src/components/ProductPageClient.tsx
+++ src/components/ProductPageClient.tsx
@@ -1051,6 +1051,7 @@ export function ProductPageClient({ product, relatedProducts, initialReviews }: 
 
         {/* Photo Reviews Section */}
         {product && (
+          <>
           <div className="mt-20 pt-16 border-t border-zinc-200">
             <div className="flex flex-col md:flex-row gap-12">
               <div className="w-full md:w-1/3">
